7e4bcee8c448bebb67ebc992a617aa6a2a268b37
wilsonmike <michaelwilsonsj@hotmail.com>
2021-04-23 14:41:11 -0400

    ready to host

--- src/app/shipped/shipped.component.ts
+++ src/app/shipped/shipped.component.ts
@@ -25,7 +25,6 @@ export class ShippedComponent implements OnInit {
     this.service.getShippedShopify().subscribe((response) => {
       this.orderShopify = response;
       this.orderShopify.orders.sort((b, a) => ((a || {}).shipDate || '').localeCompare((b || {}).shipDate || ''));
-      console.log(this.orderShopify);
     });
   }
 
@@ -33,7 +32,6 @@ export class ShippedComponent implements OnInit {
     this.service.getAnniversaryShipped().subscribe((response) => {
       this.orderAnniversary = response;
       this.orderAnniversary.orders.sort((b, a) => ((a || {}).shipDate || '').localeCompare((b || {}).shipDate || ''));
-      console.log(this.orderAnniversary);
     });
   }
 
